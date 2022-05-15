95e99f563a5b9ebd8132f10bdc6a254df1e5478d
wy8881 <wy7382@gmail.com>
2022-05-15 10:38:27 +0800

    Revert "comment out battle"
    This reverts commit 81dfd77ab03d312a0db3cef16cc459acd9074573.

--- contracts/MonsterFactory.sol
+++ contracts/MonsterFactory.sol
@@ -7,7 +7,6 @@ import "./MonsterToken.sol";
 
 contract MonsterFactory is MonsterToken{
 
-<<<<<<< Updated upstream
     event Fighter1Win(bool);
 
     struct Competitor{
@@ -24,32 +23,6 @@ contract MonsterFactory is MonsterToken{
     }
 
     MonsterLib.Statistics[] private _defaultMonsters;
-=======
-    // event Fighter1Win(bool);
-
-    // struct Competitor{
-    //     uint256 tokenId;
-    //     uint8 level;
-    //     MonsterLib.Race race;
-    //     MonsterLib.Statistics statc;
-    // }
-
-    // struct Fighter{
-    //     uint HP;
-    //     uint damage;
-    //     uint speed;
-    // }
-
-    // struct DefaultMonster{
-    //     uint HP;
-    //     uint strength;
-    //     uint speed;
-    //     uint defensive;
-    //     uint8 level;
-    // }
-
-    // DefaultMonster[] private _defaultMonsters;
->>>>>>> Stashed changes
   
     modifier onlyOwner(uint256 _tokenId1, uint256 _tokenId2){
     require(msg.sender == ownerOf(_tokenId1));
@@ -58,15 +31,9 @@ contract MonsterFactory is MonsterToken{
     }
 
     constructor() MonsterToken() {
-<<<<<<< Updated upstream
         _defaultMonsters.push(MonsterLib.Statistics(11,5,5,5));
         _defaultMonsters.push(MonsterLib.Statistics(17,7,7,7));
         _defaultMonsters.push(MonsterLib.Statistics(25,15,15,15));
-=======
-        // _defaultMonsters.push(DefaultMonster(11,5,5,5,1));
-        // _defaultMonsters.push(DefaultMonster(17,7,7,7,5));
-        // _defaultMonsters.push(DefaultMonster(25,15,15,15,10));
->>>>>>> Stashed changes
     }
 
     function getChild(uint256 _mumId, uint256 _dadId) public onlyOwner(_mumId, _dadId){
@@ -147,7 +114,6 @@ contract MonsterFactory is MonsterToken{
      * @dev Pick five competitors randomly from existing monsters
      * Cannot pick those monster whose owner is msg.sender
      */
-<<<<<<< Updated upstream
     function pickCompetitionRandomly() public view returns (Competitor[] memory){
         uint _monsterCount = totalSupply();
         Competitor[] memory competitors;
@@ -183,45 +149,6 @@ contract MonsterFactory is MonsterToken{
         return competitors;
 
     }
-=======
-    // function pickCompetitionRandomly() public view returns (Competitor[] memory){
-    //     uint _monsterCount = totalSupply();
-    //     Competitor[] memory competitors;
-    //     MonsterLib.Monster memory _currentMonster;
-    //     if( _monsterCount - ERC721.balanceOf(msg.sender) <= 5){
-    //         uint _pickedCount = 0;
-    //         for(uint i = 0; i < _monsterCount; i++){
-    //             _currentMonster = _monsterByIndex(_monsterCount);
-    //             if(ownerOf(_currentMonster.tokenId) != msg.sender){
-    //                 competitors[_pickedCount] = Competitor(_currentMonster.tokenId, _currentMonster.level, _currentMonster.race, _currentMonster.statc);
-    //                 _pickedCount ++;
-    //             }
-    //         }
-    //     }
-    //     else{
-    //         uint256[] memory _foundIndex;
-    //         uint _pickedIndex;
-    //         uint _pickedCount = 0;
-    //         uint _foundCount = 0;
-    //         uint _loops = 0;
-    //         while(_foundIndex.length < _monsterCount && competitors.length < 5 && _loops < 10){
-    //             _loops ++;
-    //             _pickedIndex = _random() % _monsterCount;
-    //             _currentMonster = _monsterByIndex(_pickedIndex);
-    //             if(!_contain(_foundIndex, _pickedIndex)){
-    //                 if(ownerOf(_currentMonster.tokenId) != msg.sender ){
-    //                     competitors[_pickedCount] = Competitor(_currentMonster.tokenId, _currentMonster.level, _currentMonster.race, _currentMonster.statc);
-    //                     _pickedCount++;
-    //                 }
-    //                 _foundIndex[_foundCount] = _pickedIndex;
-    //                 _foundCount ++;
-    //             }
-    //         }
-    //     }
-    //     return competitors;
-
-    // }
->>>>>>> Stashed changes
 
     function _contain(uint256[] memory list, uint256 elem) internal pure returns(bool){
         for(uint i = 0; i < list.length; i++){
@@ -230,7 +157,6 @@ contract MonsterFactory is MonsterToken{
         return false;
     } 
 
-<<<<<<< Updated upstream
     function battleWithPlayer(uint256 _tokenId1, uint256 _tokenId2) public  returns(bool){
         MonsterLib.Monster memory _player1 = monsterById(_tokenId1);
         MonsterLib.Monster memory _player2 = monsterById(_tokenId2);
@@ -276,57 +202,4 @@ contract MonsterFactory is MonsterToken{
         else if( turnsNeedFor1 < turnsNeedFor2) return true;
         else return attackFirst;
     }
-=======
-    // function battleWithPlayer(uint256 _tokenId1, uint256 _tokenId2) public  returns(bool){
-    //     MonsterLib.Monster memory _player1 = monsterById(_tokenId1);
-    //     MonsterLib.Monster memory _player2 = monsterById(_tokenId2);
-    //     uint _damage1 = (_player1.statc.strength/_player2.statc.defensive) + 1;
-    //     uint _damage2 = (_player2.statc.strength/_player1.statc.defensive) + 1;
-    //     Fighter memory _fighter1 = Fighter(_player1.statc.HP, _damage1, _player1.statc.speed);
-    //     Fighter memory _fighter2 = Fighter(_player2.statc.HP, _damage2, _player2.statc.speed);
-    //     bool isFighter1Win = _simulateBattle(_fighter1, _fighter2);
-    //     if(isFighter1Win){
-    //         _updateExp(_tokenId1, _player2.level * 10);
-    //     }
-    //     emit Fighter1Win(isFighter1Win);
-    //     return isFighter1Win;
-    // }
-
-    // function battleWithDefaultMonster(uint256 _tokenId, uint256 _defaultMonsterId) public returns(bool){
-    //     require(_defaultMonsterId < _defaultMonsters.length);
-    //     MonsterLib.Monster memory _player1 = monsterById(_tokenId);
-    //     DefaultMonster memory _default = _defaultMonsters[_defaultMonsterId];
-    //     uint _damage1 = (_player1.statc.strength/_default.defensive) + 1;
-    //     uint _damage2 = (_default.strength/_player1.statc.defensive) + 1;
-    //     Fighter memory _fighter1 = Fighter(_player1.statc.HP, _damage1, _player1.statc.speed);
-    //     Fighter memory _fighter2 = Fighter(_default.HP, _damage2, _default.speed);
-    //     bool isFighter1Win = _simulateBattle(_fighter1, _fighter2);
-    //     if(isFighter1Win){
-    //         _updateExp(_tokenId, _default.level * 10);
-    //     }
-    //     emit Fighter1Win(isFighter1Win);
-    //     return isFighter1Win;
-    // }
-
-    // function _simulateBattle(Fighter memory fighter1, Fighter memory fighter2) internal view returns(bool){
-    //     uint turnsNeedFor1 = (fighter2.HP + fighter1.damage - 1)/fighter1.damage;
-    //     uint turnsNeedFor2 = (fighter1.HP + fighter2.damage - 1)/fighter2.damage;
-    //     bool attackFirst;
-    //     if(fighter1.speed > fighter2.speed){
-    //         attackFirst = true;
-    //     }
-    //     else if(fighter1.speed < fighter2.speed){
-    //         attackFirst = false;
-    //     }
-    //     else{
-    //         uint dice = _random()%2;
-    //         if(dice == 0){
-    //             attackFirst = true;
-    //         }else attackFirst = false;
-    //     }
-    //     if(turnsNeedFor1 > turnsNeedFor2) return false;
-    //     else if( turnsNeedFor1 < turnsNeedFor2) return true;
-    //     else return attackFirst;
-    // }
->>>>>>> Stashed changes
 }
